584451821bb623f9b16126658f62d9d84f7dc8b9
jyyang5 <jyyang55@outlook.com>
2018-02-09 20:05:53 -0400

    a2-stage1

--- app/src/main/java/com/example/jingyunyang/password_validator/Validator.java
+++ app/src/main/java/com/example/jingyunyang/password_validator/Validator.java
@@ -6,22 +6,22 @@ package com.example.jingyunyang.password_validator;
 
 public class Validator {
     // get a rule array with flag the rule is true or false
-    static int[] getArrary(String inputPassword) {
+    static int[] getArrary(String inputPassword){
         int correctRuleArray[] = {0, 0};                     // array storing whether rule is correct
         int passedRules = 0;
-        if (!inputPassword.equals("password"))               // rule 1: check if password is "password
+        if (!inputPassword.equals( "password"))               // rule 1: check if password is "password
             correctRuleArray[0] = 1;
-        if (inputPassword.length() >= 8)                         //rule 2: check if length of password >= 8
+        if(inputPassword.length() >= 8)                         //rule 2: check if length of password >= 8
             correctRuleArray[1] = 1;
         return correctRuleArray;
     }
-
     // count the correct rules
-    static int validate(int[] array) {
+    static int validate(int[] array){
         int count = 0;
-        for (int i = 0; i < array.length; i++)
+        for(int i=0; i< array.length;i++)
             count += array[i];
         return count;
     }
 
-}
+
+    }
--- app/src/test/java/com/example/jingyunyang/password_validator/JUnitTest.java
+++ app/src/test/java/com/example/jingyunyang/password_validator/JUnitTest.java
@@ -11,39 +11,42 @@ import static org.junit.Assert.assertEquals;
 
 public class JUnitTest {
     static Validator validator;
-
     @BeforeClass
-    public static void init() {
+    public static void init()
+    {
         validator = new Validator();
     }
-
     // test for the sume of ruleArray
     @Test
-    public void r1IsCorrect() {
+    public void r1IsCorrect()
+    {
         //test if "password"
-        assertEquals(validator.validate(validator.getArrary("password")), 1);
-        assertEquals(validator.validate(validator.getArrary("passwor1")), 2);
+        assertEquals(validator.validate(validator.getArrary("password")),1);
+        assertEquals(validator.validate(validator.getArrary("passwor1")),2);
     }
 
     @Test
-    public void r2IsCorrect() {
+    public void r2IsCorrect()
+    {
         //if length >= 8
-        assertEquals(validator.validate(validator.getArrary("passwor1")), 2);
-        assertEquals(validator.validate(validator.getArrary("passwor")), 1);
+        assertEquals(validator.validate(validator.getArrary("passwor1")),2);
+        assertEquals(validator.validate(validator.getArrary("passwor")),1);
     }
 
     // test for each rule
     @Test
-    public void r3IsCorrect() {
+    public void r3IsCorrect()
+    {
         //test if "password"
-        assertEquals(validator.getArrary("password")[0], 0);
-        assertEquals(validator.getArrary("passwor1")[0], 1);
+        assertEquals(validator.getArrary("password")[0],0);
+        assertEquals(validator.getArrary("passwor1")[0],1);
     }
 
     @Test
-    public void r4IsCorrect() {
+    public void r4IsCorrect()
+    {
         //if length >= 8
-        assertEquals(validator.getArrary("passwor")[1], 0);
-        assertEquals(validator.getArrary("passwordd")[1], 1);
+        assertEquals(validator.getArrary("passwor")[1],0);
+        assertEquals(validator.getArrary("passwordd")[1],1);
     }
 }
